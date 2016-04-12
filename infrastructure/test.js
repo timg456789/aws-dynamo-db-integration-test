40ff83575dd5077015e21e5b0b00cf2b43240f7c
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-11 22:06:40 -0400

    Fix the test command - uggh what a sin. Cleanup new test dynamodb tables as part of infrastructure tests.

--- infrastructure/test.js
+++ infrastructure/test.js
@@ -1,27 +1,17 @@
 var test = require('tape');
+var factory = require('./dynamodb-factory');
+var db = factory.create();
 
-test('list tables with dynamodb factory', function (t) {
-    t.plan(2);
-
-    var factory = require('./dynamodb-factory');
-    var dynamoDb = factory.create();
-
-    dynamoDb.listTables(function (err, data) {
-        t.false(err, 'error doesnt occur when listing tables');
-        t.ok(data.TableNames, 'data has TableNames property');
-    });
-
-});
+var DELETE_PREFIX = 'integration-test-';
 
 test('create table with food-config-factory then describe table to check status is creating', function(t) {
     t.plan(4);
 
-    var factory = require('./dynamodb-factory');
     var db = factory.create();
     var foodConfigFactory = require('./food-config-factory');
 
     var uuid = require('node-uuid');
-    var tableName = uuid.v4();
+    var tableName = DELETE_PREFIX + uuid.v4();
     var primaryKey = 'consumer';
 
     var foodConfig = foodConfigFactory.create(tableName, primaryKey);
@@ -45,23 +35,24 @@ test('create table with food-config-factory then describe table to check status 
     });
 });
 
+test('delete integration-test tables', function(t) {
+    t.plan(3);
 
+    db.listTables(function (err, data) {
+        t.false(err, 'error doesnt occur when listing tables');
+        t.ok(data.TableNames, 'data has TableNames property');
 
-
-//
-//
-
-
-
-
-
-
-//db.listTables();
-//db.deleteTable();
-/*db.rebuild(function () {
-    console.log('waiting for table to be created');
-    runWhenTableIsFound(FOOD, function() {
-        console.log('inserting into table: ' + FOOD);
-        //db.insert('banana');
+        for (var i = 0; i < data.TableNames.length; i++) {
+            var name = data.TableNames[i];
+            if (name.startsWith(DELETE_PREFIX)) {
+                console.log('deleting table: ' + name);
+                var deleteTestTable = { TableName: name };
+                db.deleteTable(deleteTestTable, function(err, data) {
+                    t.ok(!err, err);
+                    console.log(data);
+                });
+            }
+        }
     });
-});*/
+
+});
